e1254934413b03846d35779fb64ef832d19ea9d3
keeb <nick@stinemates.org>
2009-02-03 21:58:05 -0800

    refactored.

---
+++ src/org/jmpc/service/SimpleMPDPlayer.java
@@ -0,0 +1,33 @@
+package org.jmpc.service;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.jmpc.socket.SimpleMPDSocket;
+
+public class SimpleMPDPlayer implements MPDPlayer {
+
+    private final Log logger = LogFactory.getLog(getClass());
+    private SimpleMPDSocket mpd;
+
+    public SimpleMPDPlayer(String host, int port) {
+        mpd = new SimpleMPDSocket(host, port);
+    }
+    
+    public void play() {
+        mpd.submit("play");
+    }
+
+    public void previous() {
+        mpd.submit("previous");
+    }
+
+    public void next() {
+        mpd.submit("next");
+    }
+
+    public void pause() {
+        mpd.submit("pause");
+    }
+
+}
